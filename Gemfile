71bc34254927c538e3e73d5e59f200e194f94786
Andrew Good <drewhgood@gmail.com>
2015-03-20 15:41:19 -0700

    Added devise gem and completed initial setup.

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem "font-awesome-rails"
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+gem 'devise'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Myapp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  # For devise added by andrew
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
